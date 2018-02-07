c7f84093dd396c5c21ffdac9a1fd1ec80443e6d5
gaoshanhu123 <shanhug2@illinois.edu>
2018-02-07 16:50:23 -0600

    answer

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,35 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
     public static void main(String[] unused) {
-        System.out.println("Ran");
+    System.out.println(urlToString("http://erdani.com/tdpl/hamlet.txt"));
     }
+
+
+/**
+ * Retrieve contents from a URL and return them as a string.
+ *
+ * @param url url to retrieve contents from
+ * @return the contents from the url as a string, or an empty string on error
+ */
+public static String urlToString(final String url) {
+    int totalCount = 0;
+    String[] answer = url.split(" ");
+    for (int count = 0; count < answer.length; count++) {
+        totalCount = count;
+    }
+    System.out.println(totalCount);
+    Scanner urlScanner;
+    try {
+        urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+    } catch (IOException e) {
+        return "";
+    }
+    String contents = urlScanner.useDelimiter("\\A").next();
+    urlScanner.close();
+    return contents;
+
+}
 }
